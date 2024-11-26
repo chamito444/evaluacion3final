ee183a6483a37a26a64cc3e5929f60c83365ed0f
chamito444 <134552889+chamito444@users.noreply.github.com>
2024-11-26 20:41:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("almacen.json");
+const router = jsonServer.router("clases.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.port || 10000;
 
